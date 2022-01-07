9182f77d563b9b2474f083c36f55c5b6ad58ef56
Andjelko Perisic <66070554+anjeyy@users.noreply.github.com>
2022-01-07 18:10:37 +0100

    WIP - blinking label done

--- src/main/java/org/anjeyy/soba/welcome/WelcomeView.java
+++ src/main/java/org/anjeyy/soba/welcome/WelcomeView.java
@@ -1,8 +1,11 @@
 package org.anjeyy.soba.welcome;
 
+import javafx.animation.Animation;
+import javafx.animation.FadeTransition;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.util.Duration;
 import org.anjeyy.soba.common.CustomStyleSheet;
 import org.anjeyy.soba.common.MainView;
 
@@ -20,8 +23,17 @@ public class WelcomeView implements CustomStyleSheet, MainView {
 
     private Label createClickInfoLabel() {
         Label label = new Label("- click to continue -");
-        label.getStyleClass().add("");
+        CustomStyleSheet.super.add(label, "entry-screen-text");
+        blink(label);
         return label;
+    }
+
+    private void blink(Label label) {
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(2.5), label);
+        fadeTransition.setFromValue(1.0);
+        fadeTransition.setToValue(0.0);
+        fadeTransition.setCycleCount(Animation.INDEFINITE);
+        fadeTransition.play();
     }
 
     private BorderPane createBorderPane() {
